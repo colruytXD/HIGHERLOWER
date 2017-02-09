b9d82fa0b99fe61e30e6bcd0d572cf36938ad85c
Tom <tomwillaert@hotmail.be>
2017-02-09 20:14:04 +0100

    Added numbers effect on main menu

---
+++ Assets/Scripts/Extra Scripts/MainMenuNumbers_Animate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuNumbers_Animate : MonoBehaviour {
+
+    Vector2 beginPos;
+    Vector2 endPos;
+
+    bool move = false;
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+        Invoke("Movement", 0.1f);
+	}
+
+	void OnDisable() 
+	{
+		
+	}
+
+	void SetInitialReferences() 
+	{
+		
+	}
+
+    void Update()
+    {
+        if(move)
+        {
+            gameObject.transform.position = transform.position - new Vector3(0, 5);
+        }
+
+        if(transform.position.y < -10)
+        {
+            gameObject.transform.position = transform.parent.GetComponent<MainMenuNumbers_Spawn>().GenerateRandomPos();
+        }
+    }
+
+    void Movement()
+    {
+        move = true;
+    }
+}
---
+++ Assets/Scripts/Extra Scripts/MainMenuNumbers_Spawn.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuNumbers_Spawn : MonoBehaviour {
+
+    [SerializeField]
+    int resX = 1920;   //This is mobile {P9}
+    [SerializeField]
+    int resY = 1080;
+    [SerializeField]
+    public int maxHeight;
+
+    [Space(10)]
+
+    [SerializeField]
+    int maxNumber = 99;
+    [SerializeField]
+    int minNumber = 0;
+
+    [Space(10)]
+
+    [SerializeField]
+    GameObject txtParent;
+    [SerializeField]
+    GameObject txtPrefab;
+    [SerializeField]
+    int maxTxts;
+
+    public List<GameObject> txts = new List<GameObject>();
+    int currentDisplayed = 0;
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+        Spawn();
+    }
+
+	void OnDisable() 
+	{
+		
+	}
+
+	void SetInitialReferences() 
+	{
+	}
+
+    public Vector3 GenerateRandomPos()
+    {
+        Vector3 randomPos = new Vector2(Random.Range(0, resY), Random.Range(resX, maxHeight));
+
+        return randomPos;
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < maxTxts; i++)
+        {
+            GameObject txt;
+            txt = txtPrefab;
+
+            GameObject.Instantiate(txtPrefab, GenerateRandomPos(), Quaternion.identity, txtParent.transform);
+            txt.GetComponent<Text>().text = Random.Range(minNumber, maxNumber).ToString();
+            txts.Add(txt);
+        }
+    }
+}
